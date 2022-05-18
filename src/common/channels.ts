5cbe9df8edadb2dee39f4a2a9f68600e1014014e
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2022-05-18 23:33:34 +0200

    refactor: wrap ipc channels in enum

--- src/common/channels.ts
+++ src/common/channels.ts
@@ -1,22 +1,24 @@
-const channels = {
-  getSettingValue: "settings/value/get",
-  setSettingValue: "settings/value/set",
-  relaunchApp: "app/relaunch",
-  getUserDataPath: "app/userDataPath",
-  getTasks: "tasks/get",
-  getTaskResponse: "tasks/get/response",
-  addTask: "tasks/add",
-  addTaskSuccess: "tasks/add/success",
-  addTaskFail: "tasks/add/fail",
-  deleteTask: "tasks/delete",
-  deleteTaskSuccess: "tasks/delete/success",
-  deleteTaskFail: "tasks/delete/fail",
-  completeTask: "tasks/complete",
-  completeTaskSuccess: "tasks/complete/success",
-  completeTaskFail: "tasks/complete/fail",
-  incompleteTask: "tasks/incomplete",
-  incompleteTaskSuccess: "tasks/incomplete/success",
-  incompleteTaskFail: "tasks/incomplete/fail"
-};
+enum CHANNELS {
+  GET_SETTING_VALUE = "settings/value/get",
+  SET_SETTING_VALUE = "settings/value/set",
+  RELAUNCH_APP = "app/relaunch",
+  GET_USER_DATA_PATH = "app/userDataPath",
+  GET_NEXT_TASK_ID = "tasks/get/next-id",
+  GET_NEXT_TASK_ID_RESPONSE = "tasks/get/next-id/response",
+  GET_TASKS = "tasks/get",
+  GET_TASKS_RESPONSE = "tasks/get/response",
+  ADD_TASK = "tasks/add",
+  ADD_TASK_SUCCESS = "tasks/add/success",
+  ADD_TASK_FAIL = "tasks/add/fail",
+  DELETE_TASK = "tasks/delete",
+  DELETE_TASK_SUCCESS = "tasks/delete/success",
+  DELETE_TASK_FAIL = "tasks/delete/fail",
+  COMPLETE_TASK = "tasks/complete",
+  COMPLETE_TASK_SUCCESS = "tasks/complete/success",
+  COMPLETE_TASK_FAIL = "tasks/complete/fail",
+  INCOMPLETE_TASK = "tasks/incomplete",
+  INCOMPLETE_TASK_SUCCESS = "tasks/incomplete/success",
+  INCOMPLETE_TASK_FAIL = "tasks/incomplete/fail"
+}
 
-export default channels;
+export default CHANNELS;
