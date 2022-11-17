a984c161550d217d3cc797e1297eac45986ff595
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2022-11-17 20:48:16 +0100

    add disable feature to button component

--- src/components/utils/Button.tsx
+++ src/components/utils/Button.tsx
@@ -1,17 +1,23 @@
 interface ButtonProps {
   onClick: () => void;
   className?: string;
+  disabled?: boolean;
   children?: string;
   isSecondary?: boolean;
 }
 
-export const Button = ({ className, onClick, children, isSecondary }: ButtonProps) => {
+export const Button = ({ className, onClick, disabled, children, isSecondary }: ButtonProps) => {
   let classes: string;
 
   if (isSecondary) {
     classes = `button-secondary ${className}`;
   } else {
     classes = `button ${className} `;
+  }
+
+  if (disabled) {
+    classes += "disabled";
+    onClick = () => null;
   }
 
   return (
